aec320c6c5b53bdca60b7d4bf2a78742ab7f09cd
coaxial <c@64b.it>
2022-02-09 10:50:09 +0100

    Find out where shellcheck is

--- .travis/install_shellcheck.sh
+++ .travis/install_shellcheck.sh
@@ -4,4 +4,5 @@
 scversion="stable" # or "v0.4.7", or "latest"
 wget -qO- "https://github.com/koalaman/shellcheck/releases/download/${scversion?}/shellcheck-${scversion?}.linux.x86_64.tar.xz" | tar -xJv
 sudo cp "shellcheck-${scversion}/shellcheck" /usr/bin/
+which shellcheck
 shellcheck --version
